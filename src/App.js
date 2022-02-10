d90c2b343bedf52baed7a52fe2f3949cf42f3d95
Jericho <jerichobantiquete@gmail.com>
2022-02-11 00:05:21 +0800

    Initial setup for react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { Routes } from "react-router-dom";
+
 function App() {
   return (
     <>
       <h1>Space Tourism Website</h1>
+      <Routes>{/* Route goes here... */}</Routes>
     </>
   );
 }
