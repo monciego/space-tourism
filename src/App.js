c267f58fb2bb0e61fad4b2b78cb8931d78a9f72c
Jericho <jerichobantiquete@gmail.com>
2022-02-11 01:56:19 +0800

    Added global styles and css variables

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Crew from "./pages/Crew/Crew";
 import Destination from "./pages/Destination/Destination";
 import Home from "./pages/Home/Home";
 import Technology from "./pages/Technology/Technology";
+import GlobalStyle from "./globalStyles/globalStlyes";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
